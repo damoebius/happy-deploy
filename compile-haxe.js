74f8c97e4600df33b0198cd69664bba3484652f0
David Mouton <damo@tamina-online.com>
2016-09-27 18:34:27 +0200

    add CompileHaxeTask

---
+++ compile-haxe.js
@@ -0,0 +1,36 @@
+/**
+ * Created by david on 27/09/16.
+ */
+var process = require('child_process');
+
+var CompileHaxeTask = function (main,output) {
+    this.dir = '.';
+    this.src = [];
+    this.libs = [];
+    this.output = output;
+    this.main = main;
+};
+
+CompileHaxeTask.prototype.run = function (executeNextStep) {
+    console.log('compile haxe to ' + this.dir);
+    var args = ['node node_modules/haxe/bin/haxe'];
+    args.push('-main ' + this.main + ' -js ' + this.output);
+    for (var i = 0; i < this.src.length; i++) {
+        args.push('-cp '+this.src[i]);
+    }
+    for (var i = 0; i < this.libs.length; i++) {
+        args.push('-cp node_modules/'+this.libs[i]);
+    }
+
+    var options = {
+        cwd:this.dir,
+        encoding:'utf8',
+        stdio:[0,1,2]
+    };
+
+    process.execSync(args.join(' '),options);
+    executeNextStep();
+
+};
+
+module.exports.CompileHaxeTask = CompileHaxeTask;
--- deploy.js
+++ deploy.js
@@ -2,3 +2,32 @@
  * Created by david on 27/09/16.
  */
 console.log('Happy Deploy System');
+var exitTask = {
+    run:function(){
+        console.log("END");
+        process.exit(0);
+    }
+}
+
+var currentTaskIndex=0;
+var taskList = [];
+
+var executeNextStep = function(){
+    currentTaskIndex++;
+    taskList[currentTaskIndex].run(executeNextStep);
+};
+
+var run = function(){
+    addTask(exitTask);
+    taskList[0].run(executeNextStep);
+}
+
+var addTask = function(task){
+    taskList.push(task);
+}
+
+module.exports = {
+    run:run,
+    addTask:addTask
+}
+module.exports.CompileHaxeTask = require('./compile-haxe').CompileHaxeTask;
